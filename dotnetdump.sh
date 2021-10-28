75c7fb8a5f630803d64c7e98a1ae8e51916101cb
Agafonov Andrey <a.agafonov@smartcat.ai>
2021-10-28 09:37:45 +0300

    add gcdump

--- dotnetdump.sh
+++ dotnetdump.sh
@@ -11,15 +11,48 @@ apt-get update && apt-get install dotnet-sdk-3.1 -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -f awscliv2.zip
 ./aws/install
-dotnet tool install --global dotnet-dump
+dotnet tool install --global dotnet-dump dotnet-gcdump
 
 pathToDump=/tmp/dumps/
 mkdir $pathToDump
-file_name=${HOSTNAME}_core_$(date '+%Y%m%d-%H%M%S')
-fullPath=$pathToDump$file_name
+TS=$(date '+%Y%m%d-%H%M%S')
+file_name_core=${HOSTNAME}_core_$TS
+file_name_gc=${HOSTNAME}_gc_$TS
+file_name_meta=${HOSTNAME}_meta_$TS
+fullPathCore=$pathToDump$file_name_core
+fullPathGc=$pathToDump$file_name_gc
+fullPathMeta=$pathToDump$file_name_meta
 
-/root/.dotnet/tools/dotnet-dump collect -p 1 --output $fullPath
-gzip $fullPath
-aws s3 cp  $fullPath.gz  s3://smartcat-web-dumps/ --no-progress
-#--acl public-read
-rm $fullPath.gz
+START=$(date +%s%N)
+/root/.dotnet/tools/dotnet-dump collect -p 1 --output $fullPathCore
+ENDDUMP=$(date +%s%N)
+gzip $fullPathCore
+ENDARC=$(date +%s%N)
+aws s3 cp  $fullPathCore.gz  s3://smartcat-web-dumps/ --no-progress
+ENDUPLOAD=$(date +%s%N)
+rm $fullPathCore.gz
+DIFFDUMP=$(($($ENDDUMP - $START)/1000000))
+DIFFARC=$(($($ENDARC - $ENDDUMP)/1000000))
+DIFFUPLOAD=$(($($ENDUPLOAD - $ENDARC)/1000000))
+echo "dotnet-dump: Time to dump $DIFFDUMP" >$fullPathMeta
+echo "dotnet-dump: Time to dump $DIFFARC" >>$fullPathMeta
+echo "dotnet-dump: Time to dump $DIFFUPLOAD" >>$fullPathMeta
+
+START=$(date +%s%N)
+/root/.dotnet/tools/dotnet-gcdump collect -p 1 --output $fullPathGc
+ENDDUMP=$(date +%s%N)
+gzip $fullPathGc
+ENDARC=$(date +%s%N)
+aws s3 cp  $fullPathGc.gz  s3://smartcat-web-dumps/ --no-progress
+ENDUPLOAD=$(date +%s%N)
+rm $fullPathGc.gz
+DIFFDUMP=$(($($ENDDUMP - $START)/1000000))
+DIFFARC=$(($($ENDARC - $ENDDUMP)/1000000))
+DIFFUPLOAD=$(($($ENDUPLOAD - $ENDARC)/1000000))
+echo "dotnet-gcdump: Time to dump $DIFFDUMP" >>$fullPathMeta
+echo "dotnet-gcdump: Time to dump $DIFFARC" >>$fullPathMeta
+echo "dotnet-gcdump: Time to dump $DIFFUPLOAD" >>$fullPathMeta
+
+gzip $fullPathMeta
+aws s3 cp  $fullPathMeta.gz  s3://smartcat-web-dumps/ --no-progress
+rm $fullPathMeta.gz
